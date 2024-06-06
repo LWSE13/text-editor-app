6a6d92e523288328a8e95d271e85d3d8d98ad623
LWSE13 <leewheeler888@gmail.com>
2024-06-07 00:31:01 +0100

    added logic to handle asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+const assetCache = new StaleWhileRevalidate({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+registerRoute(({ request }) => request.destination === 'style' || request.destination === 'script', assetCache);
